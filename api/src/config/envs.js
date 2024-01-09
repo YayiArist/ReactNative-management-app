3a15eeb41fe8120b10a9186f1de462bb2ccd8e67
Yairys Aristigueta <yayi.arist.hz@gmail.com>
2024-01-09 14:15:24 -0300

    first commit

--- api/src/config/envs.js
+++ api/src/config/envs.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports={
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://admin:admin0@mindful.1dep9ra.mongodb.net/',
+    MONGO_URI: process.env.MONGO_URI, 
     PORT:process.env.PORT || 3000
 }
